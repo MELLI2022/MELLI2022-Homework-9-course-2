be7245fdef73d064c982468d3f1c86e799680a45
MELLI2022 <denisenkoswetlana109@gmail.com>
2023-04-11 20:11:26 +0300

    Мой первый коммит.

---
+++ src/main/java/skypro/homework9course2/Homework9Course2Application.java
@@ -0,0 +1,13 @@
+package skypro.homework9course2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework9Course2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework9Course2Application.class, args);
+    }
+
+}
